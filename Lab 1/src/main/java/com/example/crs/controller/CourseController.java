d8306eabab7466378d1dfdb31e93805051a81c4e
Hung Dinh <hung.dq1997hn@gmail.com>
2023-02-04 00:34:08 -0800

    seventh commit

--- Lab 1/src/main/java/com/example/crs/controller/CourseController.java
+++ Lab 1/src/main/java/com/example/crs/controller/CourseController.java
@@ -2,12 +2,18 @@ package com.example.crs.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +29,17 @@ public class CourseController {
 	@Autowired //This annotation allow object to be there when you start the server, dont need to new this object
 	CourseRepositoty courseRepo;
 	
+	@PostMapping("/courses")
+	public ResponseEntity<Course> createCourse(@RequestBody Course course){
+		try {
+		Course newCourse = new Course(course.getCode(), course.getTitle());
+		courseRepo.save(newCourse);
+		} catch(Exception e) {
+			return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return null;
+		
+	}
 	@GetMapping("/courses")
 	public ResponseEntity<List<Course>> getAllCourses(@RequestParam(required=false) String title) {
 		
@@ -44,4 +61,52 @@ public class CourseController {
 			return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+	
+	@GetMapping("/courses/{id}")
+	public ResponseEntity<Course> getCourseById(@PathVariable("id") long id) {
+		Optional<Course> courseData = courseRepo.findById(id);
+
+		if (courseData.isPresent()) {
+			return new ResponseEntity<>(courseData.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@PutMapping("/courses/{id}")
+	public ResponseEntity<Course> updateCourse(@PathVariable("id") long id, @RequestBody Course course) {
+		Optional<Course> courseData = courseRepo.findById(id);
+
+		if (courseData.isPresent()) {
+			Course _course = courseData.get();
+			_course.setCode(course.getCode());
+			_course.setTitle(course.getTitle());
+			return new ResponseEntity<>(courseRepo.save(_course), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@DeleteMapping("/courses")
+	public ResponseEntity<HttpStatus> deleteAllTutorials() {
+		try {
+			courseRepo.deleteAll();
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+
+	@DeleteMapping("/courses/{id}")
+	public ResponseEntity<HttpStatus> deleteCourse(@PathVariable("id") long id) {
+		try {
+			courseRepo.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+
 }
